b397ba37099fe1ae10f5d3eda5fad5149ed13204
Jalen Mnene <mnenejalen72@gmail.com>
2024-05-29 16:50:40 +0300

    Finished deliverables

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        
+        return [candidate for candidate in possible_anagrams if sorted(candidate) == sorted_word]
